5ca3ae30617b9e38ce9f047993dc9bde8f444b08
hector <hpulido1021@gmail.com>
2022-03-28 22:47:19 -0700

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(function (array) { 
+        return array.toLowerCase() === string.toLowerCase() })
+  }
+
+  function fuzzyMatch(array, string ){
+   return array.filter(function (array){
+    return array.toLowerCase().substring(0, string.length) === string.toLowerCase()
+   })
+  }
+
+function  matchName( array , string ){
+    return array.filter(function (array){
+        return array.name === string
+    })
+}
